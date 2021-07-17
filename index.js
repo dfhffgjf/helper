c24da540baae775d4da574997f409e4684afeb6d
TitovMaximDPG <titovmaximdpg@gmail.com>
2021-07-17 13:10:11 +0500

    1

---
+++ index.js
@@ -0,0 +1,24 @@
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '3253341138222259',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v11.0'
+    });
+
+    FB.AppEvents.logPageView();
+
+    FB.getLoginStatus(resp => {
+        if(resp.status === 'connected')
+            console.log('U are connected')
+    })
+
+};
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
