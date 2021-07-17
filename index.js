5724f17f0998803e6c8bc267f022f14368511b21
TitovMaximDPG <titovmaximdpg@gmail.com>
2021-07-17 15:14:49 +0500

    1

--- index.js
+++ index.js
@@ -64,16 +64,7 @@ window.fbAsyncInit = function() {
         const json = await respMedia.json();
         console.log(respMedia, json)
 
-        if(respMedia.ok) {
-            const respPost = await fetch(`https://graph.facebook.com/${bussines_accounts.id}/media_publish?creation_id=${json.id}`, {
-                method: "POST"
-            });
 
-            console.log(respPost, (await respPost.json()))
-
-            if(respPost.ok)
-                console.log('EBAT')
-        }
 
     })
 
