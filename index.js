0a471d2f47293fced1bd87833f4bf79f1ad912fd
TitovMaximDPG <titovmaximdpg@gmail.com>
2021-07-19 13:34:40 +0500

    2

--- index.js
+++ index.js
@@ -49,10 +49,10 @@ window.fbAsyncInit = function() {
             return false
         }
 
-        const date = document.getElementById("date").value;
+        const date = Date.parse(document.getElementById("date").value.toString()) - Date.now();
         console.log(date)
 
-        if(date && false) {
+        if(date && date > 0) {
             const videoURL = "https://cdn.videvo.net/videvo_files/video/premium/video0238/small_watermarked/06_day_part_II_729_wide_lednik_preview.mp4";
             const respUser = await fetch(`https://graph.facebook.com/v11.0/${bussines_accounts.id}?fields=ig_id,username,profile_picture_url&access_token=${user.accessToken}`);
 
@@ -75,18 +75,21 @@ window.fbAsyncInit = function() {
 
                 const jsonMediaID = json.id;
 
-                const intervalSetup = setInterval(async () => {
-                    const respPublish = await fetch(`https://graph.facebook.com/${bussines_accounts.id}/media_publish?creation_id=${jsonMediaID}&access_token=${user.accessToken}`, {
-                        method: "POST"
-                    })
+                const awaitTime = setInterval(() => {
+                    clearInterval(awaitTime)
+                    const intervalSetup = setInterval(async () => {
+                        const respPublish = await fetch(`https://graph.facebook.com/${bussines_accounts.id}/media_publish?creation_id=${jsonMediaID}&access_token=${user.accessToken}`, {
+                            method: "POST"
+                        })
 
-                    const json = await respPublish.json();
+                        const json = await respPublish.json();
 
-                    console.log(respPublish, json)
+                        console.log(respPublish, json)
 
-                    if(respPublish.ok)
-                        clearInterval(intervalSetup)
-                }, 5000)
+                        if(respPublish.ok)
+                            clearInterval(intervalSetup)
+                    }, 5000)
+                }, date)
             }
         }
 
