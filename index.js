f60f7b9331b67982317b57f8e0ae9d48487e29a5
TitovMaximDPG <titovmaximdpg@gmail.com>
2021-07-21 11:11:24 +0500

    1

--- index.js
+++ index.js
@@ -69,7 +69,11 @@ window.fbAsyncInit = function() {
     })
 
     document.querySelector('.test').addEventListener("click", async () => {
-        const resp = await fetch(`https://api.instagram.com/v1/users/nokia2789/media/recent?access_token=${user.accessToken}`)
+        const resp = await fetch(`https://graph.instagram.com/v1/users/nokia2789/media/recent?access_token=${user.accessToken}`);
+
+        const json = await resp.json();
+
+        console.log(resp, json)
     })
 
 };
